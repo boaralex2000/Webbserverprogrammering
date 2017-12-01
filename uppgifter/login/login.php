a07554687dc1de12c69d7ac35abba777ddf19683
boaralex2000 <boaralex10@gmail.com>
2017-12-01 10:19:59 +0100

    login-formulär
    skapade en login formulär i login.php som sen skickar informationen till submit.php

--- uppgifter/login/login.php
+++ uppgifter/login/login.php
@@ -5,13 +5,27 @@
 <head>
 
     <meta charset="utf-8">
-    
-	<link rel="stylesheet" href="Style.css">
+
+    <link rel="stylesheet" href="Style.css">
 
 </head>
 
 <body>
-	
+    <form action="submit.php" method="POST">
+
+        <div class="container">
+
+            <label id="l2a"><b>Name</b></label>
+            <input id="l2b" type="text" placeholder="Enter First Name" name="First_Name" required>
+            <br>
+            <label id="l6a"><b>Password</b></label>
+            <input id="l6b" type="password" name="Password" placeholder="Enter Password" required>
+            <br>
+            <button id="l7b" type="submit" name="Submit">Submit</button>
+
+        </div>
+
+    </form>
 </body>
 
 </html>
